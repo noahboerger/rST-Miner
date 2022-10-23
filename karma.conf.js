c81d5014399d080fefe0cedb411d18c623dc1983
noahboerger <noahboerger@gmail.com>
2022-10-23 22:11:38 +0200

    Update

--- karma.conf.js
+++ karma.conf.js
@@ -25,12 +25,19 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fapra-template'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+        dir: require('path').join(__dirname, './coverage/fapra-template'),
+        subdir: '.',
+        reporters: [
+            {type: 'lcov'}
+        ],
+        check: {
+            global: {
+                statements: 80,
+                branches: 80,
+                functions: 80,
+                lines: 80
+            }
+        }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
