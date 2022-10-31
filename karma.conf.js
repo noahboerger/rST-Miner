a418352083a322cc90ffda75cbd71f92334f34f0
noahboerger <noahboerger@gmail.com>
2022-10-31 23:03:44 +0100

    Fix

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-        dir: require('path').join(__dirname, './coverage/eST-Miner'),
+        dir: require('path').join(__dirname, './coverage/rST-Miner'),
         subdir: '.',
         reporters: [
             {type: 'lcov'}
