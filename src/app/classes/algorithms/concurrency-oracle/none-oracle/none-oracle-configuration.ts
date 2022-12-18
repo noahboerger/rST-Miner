f1305a5ee4e591c5f925202ecc8608f0e42c4a78
noahboerger <35099943+noahboerger@users.noreply.github.com>
2022-12-18 19:00:32 +0100

    Erweiterung NoneOracle-Config (#21)

---
+++ src/app/classes/algorithms/concurrency-oracle/none-oracle/none-oracle-configuration.ts
@@ -0,0 +1,3 @@
+export interface NoneOracleConfiguration {
+    distinguishSameLabels?: boolean;
+}
--- src/app/classes/algorithms/concurrency-oracle/none-oracle/none-oracle.ts
+++ src/app/classes/algorithms/concurrency-oracle/none-oracle/none-oracle.ts
@@ -2,9 +2,24 @@ import { ConcurrencyOracle } from '../concurrency-oracle';
 import { ConcurrencyRelation } from '../../../models/concurrency/concurrency-relation';
 import { TraceCleaner } from '../trace-cleaner';
 import { Eventlog } from '../../../models/eventlog/eventlog';
+import { Relabeler } from '../../../utility/relabeler';
+import { NoneOracleConfiguration } from './none-oracle-configuration';
 
 export class NoneOracle extends TraceCleaner implements ConcurrencyOracle {
+    constructor(private config: NoneOracleConfiguration = {}) {
+        super();
+    }
+
     determineConcurrency(eventlog: Eventlog): ConcurrencyRelation {
-        return ConcurrencyRelation.noConcurrency();
+        const eventlogTraces = eventlog.traces;
+
+        if (eventlogTraces.length === 0 || !this.config.distinguishSameLabels) {
+            return ConcurrencyRelation.noConcurrency();
+        }
+
+        const cleanedLog = this.cleanLog(eventlogTraces);
+        const relabeler = new Relabeler();
+        relabeler.uniquelyRelabelSequences(cleanedLog);
+        return ConcurrencyRelation.noConcurrency(relabeler);
     }
 }
