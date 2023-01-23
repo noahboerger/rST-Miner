9ae1ffe79e331054c6ef8f297c481043b261eee4
noahboerger <noahboerger@gmail.com>
2023-01-23 19:08:46 +0100

    Remove unnecessary Workflow-Characteristics-Config

--- src/app/classes/models/miner-settings/process-model-characteristics-config.ts
+++ src/app/classes/models/miner-settings/process-model-characteristics-config.ts
@@ -4,19 +4,13 @@ import { jsonMember, jsonObject } from 'typedjson';
 @jsonObject
 export class ProcessModelCharacteristicsConfig {
     public static readonly DEFAULT_PLACES_EMPTY_AT_END = true;
-    public static readonly DEFAULT_WORKFLOW_NET = false;
 
     @jsonMember(Boolean)
     public placesEmptyAtEnd: boolean;
 
-    @jsonMember(Boolean)
-    public workflowNet: boolean;
-
     constructor(
         placesEmptyAtEnd: boolean = ProcessModelCharacteristicsConfig.DEFAULT_PLACES_EMPTY_AT_END,
-        workflowNet: boolean = ProcessModelCharacteristicsConfig.DEFAULT_WORKFLOW_NET
     ) {
         this.placesEmptyAtEnd = placesEmptyAtEnd;
-        this.workflowNet = workflowNet;
     }
 }
